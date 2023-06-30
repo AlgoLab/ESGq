20f609d5dadd8a79e3cd53714d806952ee6ac0a7
Luca Denti <ldenti92@gmail.com>
2023-06-30 16:31:05 +0200

    Add script to do correlation plots

--- exps/scripts/plot_corr.py
+++ exps/scripts/plot_corr.py
@@ -2,67 +2,71 @@ import sys
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-
 from scipy.stats import pearsonr
+
+plt.rcParams.update({"font.size": 13})
 
 
 def main():
     csv_path = sys.argv[1]
     prefix = sys.argv[2]
 
-    df = pd.read_csv(csv_path)
+    fulldf = pd.read_csv(csv_path)
+    fulldf = fulldf[
+        fulldf["ESGq"].notna() & fulldf["rMATS"].notna() & fulldf["SUPPA2"].notna()
+    ]
+    for k in fulldf["k"].unique():
+        print(k)
+        df = fulldf[fulldf["k"] == k]
+        print(k, len(df))
+        corr, _ = pearsonr(df["ESGq"], df["rMATS"])
+        corr = round(corr, 3)
+        g = sns.jointplot(
+            data=df,
+            x="ESGq",
+            y="rMATS",
+            hue="EventType",
+            kind="scatter",
+            xlim=(-1.05, 1.05),
+            ylim=(-1.05, 1.05),
+        )
+        plt.text(s=f"Pearson correlation: {corr}", x=-0.3, y=-1)
+        plt.tight_layout()
+        plt.savefig(prefix + f".k{k}.ESGq-vs-rMATS.png")
+        plt.close()
 
-    df = df[df["ESGq"].notna() & df["rMATS"].notna() & df["SUPPA2"].notna()]
+        corr, _ = pearsonr(df["ESGq"], df["SUPPA2"])
+        corr = round(corr, 3)
+        sns.jointplot(
+            data=df,
+            x="ESGq",
+            y="SUPPA2",
+            hue="EventType",
+            kind="scatter",
+            xlim=(-1.05, 1.05),
+            ylim=(-1.05, 1.05),
+            legend=None,
+        )
+        plt.text(s=f"Pearson correlation: {corr}", x=-0.3, y=-1)
+        plt.tight_layout()
+        plt.savefig(prefix + f".k{k}.ESGq-vs-SUPPA2.png")
+        plt.close()
 
-    corr, _ = pearsonr(df["ESGq"], df["rMATS"])
-    corr = round(corr, 3)
-    g = sns.jointplot(
-        data=df,
-        x="ESGq",
-        y="rMATS",
-        hue="EventType",
-        kind="scatter",
-        xlim=(-1.05, 1.05),
-        ylim=(-1.05, 1.05),
-    )
-    plt.text(s=f"Pearson correlation: {corr}", x=0, y=-1)
-
-    plt.tight_layout()
-    plt.savefig(prefix + ".ESGq-vs-rMATS.png")
-    plt.close()
-
-    corr, _ = pearsonr(df["ESGq"], df["SUPPA2"])
-    corr = round(corr, 3)
-    sns.jointplot(
-        data=df,
-        x="ESGq",
-        y="SUPPA2",
-        hue="EventType",
-        kind="scatter",
-        xlim=(-1.05, 1.05),
-        ylim=(-1.05, 1.05),
-        legend=None,
-    )
-    plt.text(s=f"Pearson correlation: {corr}", x=0, y=-1)
-    plt.tight_layout()
-    plt.savefig(prefix + ".ESGq-vs-SUPPA2.png")
-    plt.close()
-
-    corr, _ = pearsonr(df["rMATS"], df["SUPPA2"])
-    corr = round(corr, 3)
-    sns.jointplot(
-        data=df,
-        x="rMATS",
-        y="SUPPA2",
-        hue="EventType",
-        kind="scatter",
-        xlim=(-1.05, 1.05),
-        ylim=(-1.05, 1.05),
-        legend=None,
-    )
-    plt.text(s=f"Pearson correlation: {corr}", x=0, y=-1)
-    plt.tight_layout()
-    plt.savefig(prefix + ".rMATS-vs-SUPPA2.png")
+        corr, _ = pearsonr(df["rMATS"], df["SUPPA2"])
+        corr = round(corr, 3)
+        sns.jointplot(
+            data=df,
+            x="rMATS",
+            y="SUPPA2",
+            hue="EventType",
+            kind="scatter",
+            xlim=(-1.05, 1.05),
+            ylim=(-1.05, 1.05),
+            legend=None,
+        )
+        plt.text(s=f"Pearson correlation: {corr}", x=-0.3, y=-1)
+        plt.tight_layout()
+        plt.savefig(prefix + f".k{k}.rMATS-vs-SUPPA2.png")
 
 
 if __name__ == "__main__":
