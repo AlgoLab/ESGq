8c8cd771a3062c67236c2b7ca3f4529da9684c4c
dlcgold <dlcgold96@gmail.com>
2023-07-01 15:23:48 +0200

    add per event correlation table

--- exps/scripts/get_corr_table.py
+++ exps/scripts/get_corr_table.py
@@ -11,7 +11,7 @@ def get_corr(df, t1, t2):
     return corr
 
 
-def dump_from(csv_path, l, label):
+def dump_from(csv_path, l, label, eb=False):
     fulldf = pd.read_csv(csv_path)
     fulldf = fulldf[
         fulldf["ESGq"].notna() & fulldf["rMATS"].notna() & fulldf["SUPPA2"].notna()
@@ -26,6 +26,19 @@ def dump_from(csv_path, l, label):
         corr = get_corr(df, "rMATS", "SUPPA2")
         print(label, l, "\\rmats", f"\\suppa (k{k})", corr, sep=" & ", end=" \\\\\n")
 
+    if eb:
+        print()
+        df2 = fulldf[fulldf["k"] == 31]
+        for event in ["SE", "A3", "A5", "RI"]:
+            df = df2[df2["EventType"] == event]
+            corr = get_corr(df, "ESGq", "rMATS")
+            print(event, "\\esgq", "\\rmats", corr, sep=" & ", end=" \\\\\n")
+            corr = get_corr(df, "ESGq", "SUPPA2")
+            print(event, "\\esgq", f"\\suppa (k{k})", corr, sep=" & ", end=" \\\\\n")
+            corr = get_corr(df, "rMATS", "SUPPA2")
+            print(event, "\\rmats", f"\\suppa (k{k})", corr, sep=" & ", end=" \\\\\n")
+        print()
+
 
 def main():
     csv_path_1 = sys.argv[1]  # 50, PE
@@ -35,7 +48,7 @@ def main():
 
     dump_from(csv_path_1, 50, "PE")
     dump_from(csv_path_2, 100, "PE")
-    dump_from(csv_path_3, 150, "PE")
+    dump_from(csv_path_3, 150, "PE", True)
     dump_from(csv_path_4, 150, "SE")
 
 
